19ae0f3b14e0a0f324e8c99bf7591cf7eb431d7e
Obidjon <obidjon961@gmail.com>
2024-12-17 14:23:44 +0300

    stroki2

--- bin/stroki.dart
+++ bin/stroki.dart
@@ -2,5 +2,15 @@
 
 void main(List<String> arguments) {
   String name = 'Maksim';
-  print(name);
+  // Печатаем строку в консоль
+  // print(name);
+  
+  // Получаем из строки значение по индексу
+  // print(name[0]);
+  
+  // Получаем из строки значение по индексу
+  // print(name[1]);
+  
+  // Получаем длина строки
+  // print(name.length);
 }
