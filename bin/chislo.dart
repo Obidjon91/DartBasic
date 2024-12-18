a2aa8103a33b1f3c529ae508198ffc9b02757899
Obidjon91 <obidjon961@gmail.com>
2024-12-18 06:41:18 +0300

    end stroki

--- bin/chislo.dart
+++ bin/chislo.dart
@@ -2,33 +2,48 @@
 double a = 10.4;
 double b = -20.25;
 double c = 30.6;
+double f = 40.7333;
 int d = 10;
 int e = 9;
+
 void main(){
 
   //  Берем значение по модулю
-  // print(b);
-  // print(b.abs());
+  print(b);
+  print(b.abs());
   // Округление до большего и меньшего ближайшего целого
-  // print(a);
-  // print(a.ceil());
-  // print(a.floor());
+  print(a);
+  print(a.ceil());
+  print(a.floor());
 
   // Округление до ближайшего целого
-  // print(a.round());
+  print(a.round());
 
   // Число четное или нет?
-  // print(d.isEven);
-  // print(e.isEven);
+  print(d.isEven);
+  print(e.isEven);
 
   // Число нечетное или нет?
-  // print(d.isOdd);
-  // print(e.isOdd);
+  print(d.isOdd);
+  print(e.isOdd);
 
   //  Число отрицательное или нет?
-  // print(b.isNegative);
-  // print(e.isNegative);
+  print(b.isNegative);
+  print(e.isNegative);
 
+  // Уменьшить количество цифр послей запятой
+  print(f);
+  print(f.toStringAsFixed(2));
+  print(f.toStringAsFixed(3));
+  print(f.toStringAsFixed(4));
+  print(f.toStringAsFixed(5));
+  print(f.toStringAsFixed(6));
+
+
+  // Заполнить нулями слева и справа
+  int chislo = 0000001;
+  print(chislo.toString().padLeft(10, '0'));
+  print(chislo.toString().padRight(10, '0'));
   
 
 }
--- bin/stroki.dart
+++ bin/stroki.dart
@@ -2,15 +2,37 @@
 
 void main(List<String> arguments) {
   String name = 'Maksim';
+  String lastName = 'Petrov';
   // Печатаем строку в консоль
-  // print(name);
+  print(name);
   
   // Получаем из строки значение по индексу
-  // print(name[0]);
+  print(name[0]);
   
   // Получаем из строки значение по индексу
-  // print(name[1]);
+  print(name[1]);
   
   // Получаем длина строки
-  // print(name.length);
+  print(name.length);
+  
+  // Переводим строку в верхний регистр
+  print(name.toUpperCase());
+  
+  // Переводим строку в нижний регистр
+  print(name.toLowerCase());
+  
+  // Переводим первый символ в верхний регистр
+  print(name[0].toUpperCase() + name.substring(1));
+
+  // Переводим первую букву в верхний регистр
+  print(name.replaceRange(0, 1, name[0].toUpperCase()));
+
+  // Конкатенация строк
+  print('Hello! ' + name);
+  print(lastName + ' ' + name);
+  String fullName = '$lastName $name Kto ti ara?';
+  print(fullName);
+
+  // 
+
 }
